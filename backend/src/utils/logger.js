9df999afe331476d6ce4683b68dc458c20fd0176
Nguyễn Tiến Đạt <datlacve2@gmail.com>
2025-11-12 12:08:49 +0700

    frontend

--- backend/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = {
-  log: (...args) => console.log('[LOG]', ...args),
-  info: (...args) => console.log('[INFO]', ...args),
-  error: (...args) => console.error('[ERROR]', ...args),
-};
-
-module.exports = logger;
-  
